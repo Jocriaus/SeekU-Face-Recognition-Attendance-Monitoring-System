322f8a6b9d846c34cf59ad7ca1d1bacd4acc821a
systemeror12 <vrixenmendoza@gmail.com>
2022-10-23 19:05:11 +0800

    File added to Face recog
    adding some files

--- FaceDetection/face_detection.py
+++ FaceDetection/face_detection.py
@@ -1,1 +1,24 @@
 import cv2 as cv
+import numpy as np
+
+haar_face_cascade = cv.CascadeClassifier(
+    "FaceDetection/data/haarcascade_frontalface_alt.xml"
+)
+
+cap = cv.VideoCapture(0)
+while True:
+    _, img = cap.read()
+
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    faces = haar_face_cascade.detectMultiScale(gray, 1.1, 6)
+
+    for (x, y, w, h) in faces:
+        cv.rectangle(img, (x, y), (x + w, y + h), (0, 0, 250), 2)
+
+    cv.imshow("Detect Face Image", img)
+    k = cv.waitKey(30) & 0xFF
+    if k == 27:
+        break
+
+cap.release()
