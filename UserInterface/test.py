81c47c74cf17e1f89ea83e37df32f5c01cf97ed0
Jocriaus <austriajc30@gmail.com>
2022-10-26 17:19:32 +0800

    made a test ui

--- UserInterface/test.py
+++ UserInterface/test.py
@@ -1,1 +1,14 @@
-print("hello")
+from tkinter import *
+
+#tk name of window
+root = Tk()
+
+# Creating a Label Widget
+myLabel1 = Label(root, text= "Hello World!")
+myLabel2 = Label(root, text= "My name is  JC!").grid(row=0, column=1)
+# putting the widget to root (screen)
+myLabel1.grid(row=0, column=0)
+
+
+
+root.mainloop()
