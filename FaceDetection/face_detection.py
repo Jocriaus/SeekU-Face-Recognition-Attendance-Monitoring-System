77ca6ae767d7a10e03d65a5602d48581693d1197
systemeror12 <vrixenmendoza@gmail.com>
2022-10-29 13:34:42 +0800

    Update face_detection.py

--- FaceDetection/face_detection.py
+++ FaceDetection/face_detection.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-#load cascade classifier training file for haarcascade
+# load cascade classifier training file for haarcascade
 haar_face_cascade = cv.CascadeClassifier(
     "FaceDetection/data/haarcascade_frontalface_alt.xml"
 )
@@ -9,12 +9,11 @@ haar_face_cascade = cv.CascadeClassifier(
 cap = cv.VideoCapture(0)
 while True:
 
-    #img as video 
-    _,  img = cap.read()
-    #img set img as graye color
+    # img as video
+    _, img = cap.read()
+    # img set img as graye color
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    
     faces = haar_face_cascade.detectMultiScale(gray, 1.1, 6)
 
     for (x, y, w, h) in faces:
