e1a958b22fc724db761aee387cca7138dd399cc8
systemeror12 <vrixenmendoza@gmail.com>
2022-11-02 01:00:11 +0800

    Update face_detection.py

--- FaceDetection/face_detection.py
+++ FaceDetection/face_detection.py
@@ -1,27 +1,99 @@
-import cv2 as cv
+import cv2
+import os
 import numpy as np
 
-# load cascade classifier training file for haarcascade
-haar_face_cascade = cv.CascadeClassifier(
-    "FaceDetection/data/haarcascade_frontalface_alt.xml"
-)
+subjects = ["", "Jose Crisanto", "Vrixen Mendoza"]
 
-cap = cv.VideoCapture(0)
-while True:
 
-    # img as video
-    _, img = cap.read()
-    # img set img as graye color
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+def detect_face(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier(
+        "/FaceDetection/data/haarcascade_frontalface_alt.xml"
+    )
 
-    faces = haar_face_cascade.detectMultiScale(gray, 1.1, 6)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
 
-    for (x, y, w, h) in faces:
-        cv.rectangle(img, (x, y), (x + w, y + h), (0, 0, 250), 2)
+    if len(faces) == 0:
+        return None, None
 
-    cv.imshow("Detect Face Image", img)
-    k = cv.waitKey(30) & 0xFF
-    if k == 27:
-        break
+    x, y, w, h = faces[0]
 
-cap.release()
+    return gray[y : y + w, x : x + h], faces[0]
+
+
+def prepare_training_data(data_folder_path):
+    dirs = os.listdir(data_folder_path)
+
+    faces = []
+    labels = []
+
+    for dir_name in dirs:
+        if not dir_name.startswith("s"):
+            continue
+
+        label = int(dir_name.replace("s", ""))
+        subject_dir_path = data_folder_path + "/" + dir_name
+        subject_image_names = os.listdir(subject_dir_path)
+
+        for image_name in subject_image_names:
+            if image_name.startswith("."):
+                continue
+
+            image_path = subject_dir_path + "/" + image_name
+            image = cv2.imread(image_path)
+
+            cv2.imshow("Training on image...", image)
+            cv2.waitKey(100)
+
+            face, rect = detect_face(image)
+
+            if face is not None:
+                faces.append(face)
+                labels.append(label)
+
+                cv2.destroyAllWindows()
+                cv2.waitKey(1)
+                cv2.destroyAllWindows()
+    return faces, labels
+
+
+print("Preparing data...")
+faces, labels = prepare_training_data("training-data")
+print("Data prepared")
+
+print("Total faces: ", len(faces))
+print("Total labels: ", len(labels))
+
+face_recognizer = cv2.face.createEigenFaceRecognizer()
+
+face_recognizer.train(faces, np.array(labels))
+
+
+def draw_rectangle(img, rect):
+    (x, y, w, h) = rect
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+
+def draw_text(img, text, x, y):
+    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+
+
+def predict(text_img):
+    img = text_img.copy()
+    face, rect = detect_face(img)
+    label = face_recognizer.predict(face)
+    label_text = subjects[label]
+    draw_rectangle(img, rect)
+    draw_text(img, label_text, rect[0], rect[1] - 5)
+    return img
+
+
+print("Predicting images...")
+# need image
+test_img1 = cv2.imread()
+test_img2 = cv2.imread()
+
+predicted_img1 = predict(test_img1)
+predicted_img2 = predict(test_img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
