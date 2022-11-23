f92c86b7f763c995bfefb14839721f7ada14b22b
systemeror12 <vrixenmendoza@gmail.com>
2022-11-23 20:32:37 +0800

    New folder
    asdasd

---
+++ Face Recognition/FaceRecog.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+
+path = "ImagesAttendance"
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+
+for cl in myList:
+    currentImage = cv2.imread(f"{path}/{cl}")
+    images.append(currentImage)
+    classNames.append(os.path.splitext(cl)[0])
+
+print(classNames)
+
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+encodeListKnownFaces = findEncodings(images)
+
+print("Encode Complete")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgSmall = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgSmall = cv2.cvtColor(imgSmall, cv2.COLOR_BGR2RGB)
+
+    facesCurrentFrame = face_recognition.face_locations(imgSmall)
+    encodesCurrentFrame = face_recognition.face_encodings(imgSmall, facesCurrentFrame)
+
+    for encodeFace, faceLocation in zip(encodesCurrentFrame, facesCurrentFrame):
+        matches = face_recognition.compare_faces(encodeListKnownFaces, encodeFace)
+        faceDistance = face_recognition.face_distance(encodeListKnownFaces, encodeFace)
+        print(faceDistance)
+        matchIndex = np.argmin(faceDistance)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+            y1, x2, y2, x1 = faceLocation
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(
+                img,
+                name,
+                (x1 + 6, y2 - 6),
+                cv2.FONT_HERSHEY_COMPLEX,
+                1,
+                (255, 255, 255),
+                2,
+            )
+    cv2.imshow("Webcam", img)
+    k = cv2.waitKey(30) & 0xFF
+    if k == 27:
+        break
+cap.release()
--- FaceDetection/folder-creation.py
+++
@@ -1,6 +0,0 @@
-import os
-
-path = "C:\\Users\\vrixe\\Desktop\\test\\image-dataset"
-os.chdir(path)
-student_folder = input("Enter new student folder:")
-os.makedirs(student_folder)
