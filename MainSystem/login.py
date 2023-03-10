1c7af461b5a3f0a7596bced98bd8f0cec75c5308
systemeror12 <vrixenmendoza@gmail.com>
2023-03-10 19:52:20 +0800

    editing contents of login.py

--- MainSystem/login.py
+++ MainSystem/login.py
@@ -1,29 +1,18 @@
-import pyodbc as odbc
 import connection as conn
 
 
-class dbLogin:
-    def __init__(self, master=None):
-        self.db_connection = conn.dbConnect()
+class dbQuery:
+    def login_user(username_input, password_input):
 
-        self.username_input = input("Enter username: ")
-        self.password_input = input("Enter password: ")
+        db_connection = conn.dbConnect()
+        cursor = db_connection.connection.cursor()
+        query = "SELECT * FROM tbl_user WHERE username = ? AND password = ?"
 
-        self.cursor = self.db_connection.connection.cursor()
+        cursor.execute(query, (username_input, password_input))
+        user = cursor.fetchone()
+        cursor.close()
 
-        self.query = "SELECT * FROM tbl_user WHERE username = ? AND password = ?"
-        self.cursor.execute(self.query, (self.username_input, self.password_input))
-
-        self.user = self.cursor.fetchone()
-
-        if self.user is None:
-            print("Invalid username or password")
+        if user:
+            return True
         else:
-            print("Welcome, " + self.user[1] + "!")
-
-        self.cursor.close()
-        self.db_connection.connection.close()
-
-
-if __name__ == "__main__":
-    app = dbLogin()
+            return False
