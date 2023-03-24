e93d460ab8cc4149f809e38ee4369d96a50a1cc5
systemeror12 <vrixenmendoza@gmail.com>
2023-03-24 18:39:09 +0800

    added student_attendance_entry and exit

--- MainSystem/query.py
+++ MainSystem/query.py
@@ -202,6 +202,56 @@ class dbQueries:
         self.connection.commit()
         print(f"Student {student_number} has been updated successfully!")
 
+    def student_attendance_entry(
+        self, student_number, student_attendance_date, student_time_in
+    ):
+        query = f"SELECT * FROM tbl_student WHERE id = ?"
+        self.cursor.execute(query, (student_number))
+        row = self.cursor.fetchone()
+
+        if row:
+            insert_query_attendance = f"INSERT INTO tbl_student_attendance (student_no, student_attendance_date, student_time_in) VALUES (?, ?, ?)"
+            self.cursor.execute(
+                insert_query_attendance,
+                (student_number, student_attendance_date, student_time_in),
+            )
+            self.connection.commit()
+
+            join_query_attendance = f"SELECT tbl_student.student_no, tbl_student.student_firstname, tbl_student.student_lastname, tbl_student.student_middlename, tbl_student.student_contact_no, tbl_student.student_address, tbl_student.student_status, tbl_student_attendance.student_attendance_date, tbl_student_attendance.student_time_in FROM tbl_student INNER JOIN tbl_student_attendance ON tbl_student.student_no = tbl_student_attendance.student_no WHERE student.student_no = ? AND tbl_student_attendance.student_attendance_date = ?"
+
+            self.cursor.execute(
+                join_query_attendance, (student_number, student_attendance_date)
+            )
+            print("Attendance added successfully!")
+
+        else:
+            print("Student not found.")
+
+    def student_attendance_exit(
+        self, student_number, student_attendance_date, student_time_out
+    ):
+        pass
+
+    def personnel_attendance_entry(
+        self, personnel_number, personnel_attendance_date, personnel_time_in
+    ):
+        pass
+
+    def personnel_attendance_exit(
+        self, personnel_number, personnel_attendance_date, personnel_time_out
+    ):
+        pass
+
+    def visitor_attendance_entry(
+        self, visitor_number, visitor_attendance_date, visitor_time_in
+    ):
+        pass
+
+    def visitor_attendance_exit(
+        self, visitor_number, visitor_attendance_date, visitor_time_in
+    ):
+        pass
+
 
 # if db.login_entry("systemeror12", "RanOnline124"):
 # print("Login successful")
