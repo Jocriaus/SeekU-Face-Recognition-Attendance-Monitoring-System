21f70a9b3d4aa5dae16a894b0450ea5cd1972527
Jocriaus <austriajc30@gmail.com>
2023-05-15 14:46:10 +0800

    deleted main sys

--- MainSys/changechar.py
+++
@@ -1,9 +0,0 @@
-path = "C:/Users/JC Austria/Documents/GitHub/Face-Recognition-Attendance-Monitoring-System/MainSystem/user_create.py"
-newpath = ""
-for char in path:
-    if char == "/":
-        newpath += "\\"
-    else:
-        newpath += char
-
-print(newpath)
--- MainSys/print_time.py
+++
@@ -1,4 +0,0 @@
-import datetime
-
-date = datetime.datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
-print (date)
